01e3ae8e801dabcca1a0fa7fe065ec6f5dc3083d
Jasper Smith <jasper@sparks.co.uk>
2014-01-09 22:38:29 +0000

    Started styling and adding more details to details div

--- index.php
+++ index.php
@@ -6,17 +6,15 @@
 
 -->
 <html xmlns="http://www.w3.org/1999/xhtml">
-
+<head>
 <!-- Keep constants, variables and methods separate to aid readability -->
 <?php
 
 	include('appvars.php') ;
 	include('methods.php') ;
 
-?>
 
-<head>
-<?php
+
 	// set up variables
 	$location = getLocation($_POST['myLoc']) ;
 	$appTitle = makeTitle ($location) ;
@@ -32,69 +30,130 @@
 
 <title><?php echo $appTitle ; ?></title>
 <link href="weatherapp.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript">
+<!--
+	function showHide(a) {
+		// for debug
+		window.alert(a);
+		// show the detail for the element clicked; hide everything else
+		idString = "day"+0 ;
+		for (int i=0; i<=5; i++) {
+			idString = "day"+i ;
+			if (idString==a) {
+				document.getElementById(a).style.display="block";
+			} else {
+				document.getElementById(a).style.display="none";
+			}
+		}
+	}
+// -->
+</script>
 </head>
 
 <body>
-
-<form id="myForm" method="post" action="<?php echo $_SERVER['PHP_SELF'] ; ?>"> <!-- ugly first - get the thing working! -->
-
-    <p>
-    	<label for="myLoc">Location (postcode or town)</label>
-    	<input name="myLoc" type="text" id="myLoc" <?php echo formval('Oxford, UK') ; ?> />
-    </p>
+<div id="wrapper">
+    <div id="left_col">
+      <form id="myForm" method="post" action="<?php echo $_SERVER['PHP_SELF'] ; ?>"> <!-- ugly first - get the thing working! -->
+       
+        <p>
+          <label for="myLoc">Location (postcode or town)</label>
+                <input name="myLoc" type="text" id="myLoc" <?php echo formval('Oxford, UK') ; ?> />
+        </p>
+            
+            <p>
+              <input type="radio" name="myUnits" id="myUnitsI" value="Imperial" <?php echo ischecked('Imperial') ; ?> /> <label for="myUnitsI">Imperial</label><br />
+              <input type="radio" name="myUnits" id="myUnitsM" value="Metric" <?php echo ischecked('Metric') ; ?> /> <label for="myUnitsM">Metric</label><br />
+                <input name="myUnits" type="radio" id="myUnitsU" value="UK" <?php echo ischecked('UK') ; ?>/> 
+              <label for="myUnitsU">UK (Celcius, miles, mm)</label>
+        </p>
     
-    <p>
-    	<input type="radio" name="myUnits" id="myUnitsI" value="Imperial" <?php echo ischecked('Imperial') ; ?> /> <label for="myUnitsI">Imperial</label><br />
-        <input type="radio" name="myUnits" id="myUnitsM" value="Metric" <?php echo ischecked('Metric') ; ?> /> <label for="myUnitsM">Metric</label><br />
-        <input name="myUnits" type="radio" id="myUnitsU" value="UK" <?php echo ischecked('UK') ; ?>/> 
-        <label for="myUnitsU">UK (Celcius, miles, mm)</label>
-    </p>
+            <p><input type="submit" value="Get the weather!" /></p>
+            <p><a href="#" onclick="showHide('asdf')">asdf</a></p>
+      </form>
+    </div>
     
-    <p><input type="submit" value="Get the weather!" /></p>
+    <div id="right_col">
     
-</form>
-
-<p>Debug:</p>
-<p>Location: <?php echo $location ; ?><br />
-Units: <?php echo $units ; ?></p>
-<!-- Location has minimal XSS security (as this is a low-risk app); Units has same just in case. -->
-
-<?php
-	//test($requestURI);
-	// make a weather object to look examine
-	$myWeather = simplexml_load_file($requestURI);
-	
-	// set up a counter variable for use later
-	$counter = 0 ;
-		
-	foreach ($myWeather->weather as $day) {
-
-		// create the visuals for this particular day in précis form
-		echo "<div class='oneday' onmousedown='doSomething(\"day".$counter."\"'>";
-		echo "<p>";
-		echo "<img src='". $day->weatherIconUrl ."' alt='".$day->weatherDesc."' style='float:left;' />" ;
-		echo "Date: ". $day->date .": ".$day->weatherDesc."<br />" ;
-		echo "Temp max/min: ". $day->tempMaxC ."/". $day->tempMinC ."<br />" ;
-		echo "Rainfall: ". $day->precipMM ."mm<br />" ;
-		echo "</p>";
-		echo "</div>";
-
-		// create the visuals for this particular day in full form
-		echo "<div class='oneday_detail' id='day".$counter."'>";
-		echo "<p>";
-		echo "<img src='". $day->weatherIconUrl ."' alt='".$day->weatherDesc."' style='float:left;' />" ;
-		echo "Date: ". $day->date .": ".$day->weatherDesc."<br />" ;
-		echo "Temp max/min: ". $day->tempMaxC ."/". $day->tempMinC ."<br />" ;
-		echo "Rainfall: ". $day->precipMM ."mm<br />" ;
-		echo "</p>";
-		echo "</div>";
-		
-		// increment the counter
-		$counter++ ;
-	}
-
-
-?>
-
+    <?php
+        //test($requestURI);
+        // make a weather object to look examine
+        $myWeather = simplexml_load_file($requestURI);
+        //var_dump(get_object_vars($myWeather)); // debug
+    
+        // only display stuff if an object is returned without an error
+        if ($myWeather->error) {
+            echo "<p>". $myWeather->error->msg ."</p>";
+            echo "<p>Couldn't find any data for ". $location ." - please check spelling and try again</p>" ;
+            
+        } else {
+            // good to go!
+            
+            // set up a counter variable for use later
+            $counter = 0 ;
+                
+        
+            /*
+             * Now run through the days creating a summary box
+             * linked to the full details. Easiest way is to run
+             * through the list twice, in order to get the output
+             * in the right order in the document. This duplication
+             * could be ironed out at refactor stage but for this
+             * app it's scarecely worthwhile.
+             */
+            
+            // first loop: précis
+            foreach ($myWeather->weather as $day) {
+        		
+				// because of the units choice we'll have to run 
+				
+                // create the visuals for this particular day in précis form
+                echo "<div class='oneday' onmouseover='showHide(\'day".$counter."\')'>";
+                echo "<p>";
+                echo $day->date ."<br /><img src='". $day->weatherIconUrl ."' alt='".$day->weatherDesc."' />" ;
+                echo "<br />".$day->weatherDesc ;
+				
+				echo "<br />";
+				if ($units!="Imperial") {
+	                echo "Temp range: ". $day->tempMinC ." - ". $day->tempMaxC ."&deg;C<br />" ;
+				} else {
+				    echo "Temp range: ". $day->tempMinF ." - ". $day->tempMaxF ."&deg;F<br />" ;
+				}
+				
+                echo "</p>";
+                echo "<p><a href='javascript:void(0);' onclick='showHide(\'day".$counter."\'); return false;'>details</a></p>";
+                echo "</div>";
+            
+                $counter++ ;
+            } // end of first foreach
+        
+            // for layout purposes
+            echo "<br clear='all' />" ;
+        
+            // reset counter
+            $counter = 0 ;
+        
+            // second loop: detail précis
+            foreach ($myWeather->weather as $day) {
+        
+                // create the visuals for this particular day in full form
+                echo "<div class='oneday_detail' id='day".$counter."'>";
+                echo "<p>Details for ".$day->date;
+                echo "<img src='". $day->weatherIconUrl ."' alt='".$day->weatherDesc."' />" ;
+                echo "Date: ". $day->date .": ".$day->weatherDesc."<br />" ;
+                echo "Temp max/min: ". $day->tempMaxC ."/". $day->tempMinC ."<br />" ;
+                echo "Rainfall: ". $day->precipMM ."mm<br />" ;
+                echo "</p>";
+                echo "</div>";
+                
+                // increment the counter
+                $counter++ ;
+                
+            } // end of second foreach
+        } // end of main object checking if
+    
+    ?>
+    </div>
+</div>
 </body>
 </html>
--- test.php
+++ test.php
@@ -3,9 +3,21 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>API Test</title>
+<script type="text/javascript">
+<!--
+function MM_showHideLayers() { //v9.0
+  var i,p,v,obj,args=MM_showHideLayers.arguments;
+  for (i=0; i<(args.length-2); i+=3) 
+  with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
+    if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
+    obj.visibility=v; }
+}
+//-->
+</script>
 </head>
 
 <body>
+<div id="asdf" style="display:none;">ASDF</div>
 <p>Started here</p>
 <?php
 	$currentQuery = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=oxford,uk&format=xml&num_of_days=5&key=5xj4ajnzaeem6mr9b8g8ggqb' ;
@@ -13,6 +25,6 @@
 	echo "<p>Request is for ".$myWeather->request->query ." and most recent observation was at ". $myWeather->current_condition->observation_time ."</p>" ;
 ?>
 
-<p>Got to here</p>
+<p onmouseover="MM_showHideLayers('asdf','','show')">Got to here</p>
 </body>
 </html>
